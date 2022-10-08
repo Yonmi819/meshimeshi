465144564cef719d681547d9bc88f5d2928e4e51
EC2 Default User <ec2-user@ip-172-31-42-12.ap-northeast-1.compute.internal>
2022-10-08 02:40:51 +0000

    meshiterro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
     
      before_action :configure_permitted_parameters, if: :devise_controller?
+     
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
+
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
 
   protected
 
@@ -8,4 +18,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
-s
---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,40 @@
+class PostImagesController < ApplicationController
+  
+  def new #form_withに空のモデルを渡すための記述↓
+    @post_image = PostImage.new
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id 
+    @post_image.save
+    redirect_to post_images_path
+  end
+  
+  def destroy
+    @post_image = PostImage.find(params[:id]) #削除するPostImageレコードを取得
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
+  
+  
+  
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+   
+  
+end
+
+
